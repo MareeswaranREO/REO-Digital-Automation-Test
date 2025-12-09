7b4f9f102888b479a79f471d722ad6691affab82
Mareeswaran <Mareeswaran.ramar@reoditigal.com>
2025-12-09 19:11:50 +0530

    Timeout fix

--- tests/REO/Homepage.spec.js
+++ tests/REO/Homepage.spec.js
@@ -5,7 +5,7 @@ test('test', async ({ page }) => {
   await page.waitForTimeout(5000);
   await page.getByRole('button', { name: 'Accept' }).click();
   await expect (page.locator('//img[@fetchpriority="high"]')).toBeVisible();
-  await page.getByRole('list').filter({ hasText: 'February 4, 2025 GA4 Agony' }).getByRole('link').first().click();
+  await page.getByRole('link', { name: 'GA4 Agony Aunt: “Help! My GA4' }).click();
   await page.getByRole('link', { name: 'Back to search' }).click();
   await page.locator('.elementor-widget-container > a').first().click();
   await page.getByRole('link', { name: 'See more work' }).click();
@@ -20,6 +20,7 @@ test('test', async ({ page }) => {
   await page.locator('button:nth-child(5)').click();
   await page.getByRole('link', { name: 'Speak to a consultant' }).click();
   await page.goto("https://www.reodigital.com/", { waitUntil: "domcontentloaded" });
+  await page.waitForTimeout(3000);
   await page.getByRole('button', { name: 'Signup' }).click();
   await page.locator('#input_4_1_3').click();
   await page.locator('#input_4_1_3').fill('Mareeswaran');
