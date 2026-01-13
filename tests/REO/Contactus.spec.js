5ef28b13eda66d3e789794c610c07732fc9d145d
Mareeswaran <Mareeswaran.ramar@reoditigal.com>
2026-01-13 17:04:37 +0530

    contact us

---
+++ tests/REO/Contactus.spec.js
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+test('test', async ({ page }) => {
+  await page.goto('https://www.reodigital.com/');
+  await page.getByRole('button', { name: 'Accept' }).click();
+  await page.getByRole('link', { name: 'Contact' }).click();
+  await page.getByRole('textbox', { name: 'First' }).click();
+  await page.getByRole('textbox', { name: 'First' }).fill('mareeswaran');
+  await page.getByRole('textbox', { name: 'Last' }).click();
+  await page.getByRole('textbox', { name: 'Last' }).fill('ramar');
+  await page.getByRole('textbox', { name: 'Email (Required)' }).click();
+  await page.getByRole('textbox', { name: 'Email (Required)' }).fill('mareeswaran.ramar@reodigital.com');
+  await page.getByRole('textbox', { name: 'Phone (Required)' }).click();
+  await page.getByRole('textbox', { name: 'Phone (Required)' }).fill('8220184644');
+  await page.getByRole('textbox', { name: 'Company' }).click();
+  await page.getByRole('textbox', { name: 'Company' }).fill('reo');
+  await page.getByRole('textbox', { name: 'Your message (Required)' }).click();
+  await page.getByRole('textbox', { name: 'Your message (Required)' }).fill('qa');
+  });
